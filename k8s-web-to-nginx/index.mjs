c7fb2324224c56c624ebd0cb290e59cea62bf9e3
Bogdan Stashchuk <43352618+bstashchuk@users.noreply.github.com>
2023-11-28 12:50:49 +0000

    Update index.mjs

--- k8s-web-to-nginx/index.mjs
+++ k8s-web-to-nginx/index.mjs
@@ -17,6 +17,14 @@ app.get("/nginx", async (req, res) => {
   res.send(body)
 })
 
+app.get("/jsonplaceholder", async (req, res) => {
+  const url = "https://jsonplaceholder.typicode.com/todos";
+  const response = await fetch(url);
+  const body = await response.text();
+  res.setHeader("Content-Type", "application/json");
+  res.send(body);
+});
+
 app.listen(PORT, () => {
   console.log(`Web server is listening at port ${PORT}`)
 })
